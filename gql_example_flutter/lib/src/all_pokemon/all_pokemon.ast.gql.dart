6cad6a3c3c91abac6e2845838b73cd863e2a887f
Sat Mandir Khalsa <smkhalsa@Sats-MacBook-Pro.local>
2020-01-17 13:39:13 -0800

    update queries and fragments

--- gql_example_flutter/lib/src/all_pokemon/all_pokemon.ast.gql.dart
+++ gql_example_flutter/lib/src/all_pokemon/all_pokemon.ast.gql.dart
@@ -24,35 +24,11 @@ const AllPokemon = _i1.OperationDefinitionNode(
           directives: [],
           selectionSet: _i1.SelectionSetNode(selections: [
             _i1.FragmentSpreadNode(
-                name: _i1.NameNode(value: 'PokemonCard'), directives: []),
-            _i1.FieldNode(
-                name: _i1.NameNode(value: 'id'),
-                alias: null,
-                arguments: [],
-                directives: [],
-                selectionSet: null),
-            _i1.FieldNode(
-                name: _i1.NameNode(value: 'name'),
-                alias: null,
-                arguments: [],
-                directives: [],
-                selectionSet: null),
-            _i1.FieldNode(
-                name: _i1.NameNode(value: 'maxHP'),
-                alias: null,
-                arguments: [],
-                directives: [],
-                selectionSet: null),
-            _i1.FieldNode(
-                name: _i1.NameNode(value: 'image'),
-                alias: null,
-                arguments: [],
-                directives: [],
-                selectionSet: null)
+                name: _i1.NameNode(value: 'PokemonCard'), directives: [])
           ]))
     ]));
-const PokemonCard = _i1.FragmentDefinitionNode(
-    name: _i1.NameNode(value: 'PokemonCard'),
+const SubFragment = _i1.FragmentDefinitionNode(
+    name: _i1.NameNode(value: 'SubFragment'),
     typeCondition: _i1.TypeConditionNode(
         on: _i1.NamedTypeNode(
             name: _i1.NameNode(value: 'Pokemon'), isNonNull: false)),
@@ -66,6 +42,22 @@ const PokemonCard = _i1.FragmentDefinitionNode(
           selectionSet: null),
       _i1.FieldNode(
           name: _i1.NameNode(value: 'name'),
+          alias: null,
+          arguments: [],
+          directives: [],
+          selectionSet: null)
+    ]));
+const PokemonCard = _i1.FragmentDefinitionNode(
+    name: _i1.NameNode(value: 'PokemonCard'),
+    typeCondition: _i1.TypeConditionNode(
+        on: _i1.NamedTypeNode(
+            name: _i1.NameNode(value: 'Pokemon'), isNonNull: false)),
+    directives: [],
+    selectionSet: _i1.SelectionSetNode(selections: [
+      _i1.FragmentSpreadNode(
+          name: _i1.NameNode(value: 'SubFragment'), directives: []),
+      _i1.FieldNode(
+          name: _i1.NameNode(value: 'id'),
           alias: null,
           arguments: [],
           directives: [],
@@ -83,4 +75,5 @@ const PokemonCard = _i1.FragmentDefinitionNode(
           directives: [],
           selectionSet: null)
     ]));
-const document = _i1.DocumentNode(definitions: [AllPokemon, PokemonCard]);
+const document =
+    _i1.DocumentNode(definitions: [AllPokemon, SubFragment, PokemonCard]);
